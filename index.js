e06aea190a0f1a9d6e03e6ae468cbb44ca0fea4c
dotnotation <dotinmotionproductions@gmail.com>
2021-11-23 16:51:14 -0500

    commit tree 041557b8c734371c7362a1c2eb4f9b1e15739410

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array){
+    let win = array.find(record => record.result === "W")
+    if (win){
+        return win.year
+    }
+    else {
+        return undefined 
+    }
+}
